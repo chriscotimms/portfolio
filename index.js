8b32d3e113794a636a02cc30e32782186b9313c4
Chris Timms <chriscotimms@gmail.com>
2023-10-04 19:03:47 +0100

    working scrolling response to change content using js and IntersectionObserver. Menu previously using buttons, changed to a href's

--- index.js
+++ index.js
@@ -1,51 +1,74 @@
-////////////////////KEYS
+let about = document.querySelector('#about');
 
-// variable for currently focused element
-let focusNow = null;
-
-document.addEventListener('focusin', function() {
-if (focusNow !== null) {
-    //detransition from old focus
-
-    console.log("old"+focusNow.id);
-}
-  focusNow = document.activeElement;
-  // console log focus elements
-  console.log(focusNow.id);
-  //transition into new element to focus
-}, true);
-
-document.addEventListener('mouseenter', function(event) {
-    if (focusNow !== null) {
-        //detransition from old focus
-        console.log("old"+focusNow.id);
+let observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio <= 0.5) {
+      about.classList.remove("reveal");
+      about.classList.add("hide");
+        console.log("do nothing");
+      } else {
+      about.classList.add("reveal");
+      about.classList.remove("hide");
+      console.log(entry.intersectionRatio);
+      
     }
-    focusNow = event.target;
-    // console log focus elements
-    console.log(focusNow.id);
-  }, true);
+    });
+  },
+  { threshold: 0.5 }
+);
 
-// Enter key to activate focused element like mouseclick
-window.addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    focusNow.click();
-  }
-});
-
-window.addEventListener("click", function(event) {
-    console.log("click", event.target);
-  });
+observer.observe(about);
 
 
-//Esc Key to exit menus
-window.addEventListener("keydown", function(event) {
-  if (event.key === "Escape") {
-    event.preventDefault();
-    // sidebar2.classList.remove('side_bigger');
-    // sidebar1.classList.remove('side_bigger');
-    // main.classList.remove('main_smaller');
-    // setTimeout(displayAdd, 500, sidebar1);
-    // setTimeout(displayAdd, 500, sidebar2);
-  }
-});
+// ////////////////////KEYS
+
+// // variable for currently focused element
+// let focusNow = null;
+
+// document.addEventListener('focusin', function() {
+// if (focusNow !== null) {
+//     //detransition from old focus
+
+//     console.log("old"+focusNow.id);
+// }
+//   focusNow = document.activeElement;
+//   // console log focus elements
+//   console.log(focusNow.id);
+//   //transition into new element to focus
+// }, true);
+
+// document.addEventListener('mouseenter', function(event) {
+//     if (focusNow !== null) {
+//         //detransition from old focus
+//         console.log("old"+focusNow.id);
+//     }
+//     focusNow = event.target;
+//     // console log focus elements
+//     console.log(focusNow.id);
+//   }, true);
+
+// // Enter key to activate focused element like mouseclick
+// window.addEventListener("keydown", function(event) {
+//   if (event.key === "Enter") {
+//     event.preventDefault();
+//     focusNow.click();
+//   }
+// });
+
+// window.addEventListener("click", function(event) {
+//     console.log("click", event.target);
+//   });
+
+
+// //Esc Key to exit menus
+// window.addEventListener("keydown", function(event) {
+//   if (event.key === "Escape") {
+//     event.preventDefault();
+//     // sidebar2.classList.remove('side_bigger');
+//     // sidebar1.classList.remove('side_bigger');
+//     // main.classList.remove('main_smaller');
+//     // setTimeout(displayAdd, 500, sidebar1);
+//     // setTimeout(displayAdd, 500, sidebar2);
+//   }
+// });
